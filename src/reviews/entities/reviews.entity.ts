571f9da6c87ae243c7369d6be91e59b3377198f0
akmaliatrias08 <akmaliatrias08@gmail.com>
2023-11-15 13:43:29 +0700

    first commit

---
+++ src/reviews/entities/reviews.entity.ts
@@ -0,0 +1,39 @@
+import { User } from "#/users/entities/user.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Reviews {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @ManyToOne(() => User, (user) => user.reviews)
+    psycholog: User
+
+    @Column({
+        type: "int"
+    })
+    rating: number
+
+    @Column({
+        type: "text"
+    })
+    text: string
+
+    @CreateDateColumn({
+        type: 'timestamp with time zone',
+        nullable: false,
+    })
+    createdAt: Date;
+    
+    @UpdateDateColumn({
+        type: 'timestamp with time zone',
+        nullable: false,
+    })
+    updatedAt: Date;
+    
+    @DeleteDateColumn({
+        type: 'timestamp with time zone',
+        nullable: true,
+    })
+    deletedAt: Date;
+}
