51427ea64708cd1a45b8c6f996fb80706d410a4b
akmaliatrias08 <akmaliatrias08@gmail.com>
2023-12-04 10:36:44 +0700

    feat: upload profile photo user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,16 +9,45 @@ import {
   ParseUUIDPipe,
   HttpStatus,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
+  Res, 
+  Query
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '#/auth/jwt-auth.guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storageProfile } from './helpers/upload_profile';
+import { join } from 'path';
+import { of } from 'rxjs';
+
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', storageProfile))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (typeof file?.filename == "undefined") {
+        return {
+          statusCode: HttpStatus.BAD_REQUEST, 
+          message: "error file cannot be upload"
+        }
+    } else {
+        return {fileName: file?.filename}
+    }
+  }
+
+  @Get('upload/:image')
+  getImage(@Param('type') type: string,@Param('image') imagePath: string, @Res() res: any){
+    return of(
+      res.sendFile(join(process.cwd(), `upload/profile/${imagePath}`))
+    )
+  }
+  
   @Post()
   @UseGuards(JwtAuthGuard)
   async create(@Body() createUserDto: CreateUserDto) {
